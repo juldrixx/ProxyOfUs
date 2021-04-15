44c1aa3ff62905b9e3221d4cd934fbf4324317de
Juldrixx <juldrixx@gmail.com>
2021-04-15 21:12:21 +0200

    Update hat order

--- source/Patches/CustomHats/CreateHat.cs
+++ source/Patches/CustomHats/CreateHat.cs
@@ -31,7 +31,6 @@ namespace ProxyOfUs.CustomHats
             new HatData {name = "pinkee", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.4f), author="PhasmoFireGod"},
             new HatData {name = "racoon", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.4f), author="PhasmoFireGod"},
             
-            new HatData {name = "raflp", bounce = true, highUp = true, offset = new Vector2(-0.1f, 0.1f), author="??????"},
 
             new HatData {name = "aphex", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.2f), author="Nassegris"},
             new HatData {name = "junkyard", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.2f), author="Nassegris"},
@@ -41,6 +40,7 @@ namespace ProxyOfUs.CustomHats
             new HatData {name = "ze", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.2f), author="Nassegris"},
             new HatData {name = "chilled", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.2f), author="Nassegris"},
             
+            new HatData {name = "raflp", bounce = true, highUp = true, offset = new Vector2(-0.1f, 0.1f), author="??????"},
             
             new HatData {name = "harrie", bounce = true, highUp = true, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
             new HatData {name = "razz", bounce = true, highUp = true, offset = new Vector2(-0.1f, 0.3f), author="TheLastShaymin"},
@@ -68,9 +68,9 @@ namespace ProxyOfUs.CustomHats
             new HatData {name = "nonbinary", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
             
             new HatData {name = "trans_1", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
-            new HatData {name = "trans_2", bounce = false, highUp = true, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
             new HatData {name = "trans_3", bounce = false, highUp = false, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
             new HatData {name = "trans_4", bounce = false, highUp = true, offset = new Vector2(-0.1f, 0.5f), author="TheLastShaymin"},
+            new HatData {name = "trans_2", bounce = false, highUp = true, offset = new Vector2(-0.1f, 0.1f), author="TheLastShaymin"},
             
             
         };
